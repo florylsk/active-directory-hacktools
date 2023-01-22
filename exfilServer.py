d3e728f4534d2701c44555c5e2e4a267b2412dbf
Max Power <florylsk@gmail.com>
2023-01-22 05:47:28 -0500

    more tools

---
+++ exfilServer.py
@@ -0,0 +1,45 @@
+#!/usr/env python3
+import http.server
+import os
+import logging
+
+try:
+    import http.server as server
+except ImportError:
+    # Handle Python 2.x
+    import SimpleHTTPServer as server
+
+class HTTPRequestHandler(server.SimpleHTTPRequestHandler):
+    """
+    SimpleHTTPServer with added bonus of:
+
+    - handle PUT requests
+    - log headers in GET request
+    """
+
+    def do_GET(self):
+        server.SimpleHTTPRequestHandler.do_GET(self)
+        logging.warning(self.headers)
+
+    def do_PUT(self):
+        """Save a file following a HTTP PUT request"""
+        filename = os.path.basename(self.path)
+
+        # Don't overwrite files
+        if os.path.exists(filename):
+            self.send_response(409, 'Conflict')
+            self.end_headers()
+            reply_body = '"%s" already exists\n' % filename
+            self.wfile.write(reply_body.encode('utf-8'))
+            return
+
+        file_length = int(self.headers['Content-Length'])
+        with open(filename, 'wb') as output_file:
+            output_file.write(self.rfile.read(file_length))
+        self.send_response(201, 'Created')
+        self.end_headers()
+        reply_body = 'Saved "%s"\n' % filename
+        self.wfile.write(reply_body.encode('utf-8'))
+
+if __name__ == '__main__':
+    server.test(HandlerClass=HTTPRequestHandler,port=8080)
